2a3b3c0313a2ac2ca8f49b5bf610bf095504ce7f
Jesus Rojas <jesus@invorious.com>
2023-12-25 09:59:54 -0500

    feat: begin a project of marketplace :tada:

---
+++ src/main/java/com/personal/marketplaceplatzi/MarketplacePlatziApplication.java
@@ -0,0 +1,11 @@
+package com.personal.marketplaceplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplacePlatziApplication {
+	public static void main(String[] args) {
+    SpringApplication.run(MarketplacePlatziApplication.class, args);
+  }
+}
---
+++ src/test/java/com/personal/marketplaceplatzi/MarketplacePlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.marketplaceplatzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplacePlatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
