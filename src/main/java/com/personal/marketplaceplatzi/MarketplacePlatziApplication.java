5f373c8e2c12115297a247e838060a2cdb73c9c5
Jesus Rojas <jesus@invorious.com>
2023-12-25 11:21:37 -0500

    feat: add dot-env to the project :hammer:

--- src/main/java/com/personal/marketplaceplatzi/MarketplacePlatziApplication.java
+++ src/main/java/com/personal/marketplaceplatzi/MarketplacePlatziApplication.java
@@ -1,11 +1,14 @@
 package com.personal.marketplaceplatzi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketplacePlatziApplication {
 	public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("server.port", dotenv.get("PORT"));
     SpringApplication.run(MarketplacePlatziApplication.class, args);
   }
 }
